b321e7f1a63db467e61153bb463d9638abd9c3dd
DAN <life4789@gmail.com>
2022-11-01 09:48:16 +0900

    검색 결과 display

--- lib/book_service.dart
+++ lib/book_service.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'book.dart';
@@ -5,4 +6,21 @@ import 'book.dart';
 class BookService extends ChangeNotifier {
   // 책 목록
   List<Book> bookList = [];
+
+  void getBookList(String q) async {
+    bookList.clear();
+
+    Response result = await Dio().get(
+      "https://www.googleapis.com/books/v1/volumes?q=$q&startIndex=0&maxResults=40",
+    );
+
+    List items = result.data["items"];
+    for (Map<String, dynamic> item in items) {
+      Map<String, dynamic> volumeInfo = item["volumeInfo"];
+      Book book = Book.fromJson(volumeInfo);
+      bookList.add(book);
+    }
+
+    notifyListeners();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: sort_child_properties_last
 
+import 'package:book_store/book.dart';
 import 'package:book_store/book_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -27,61 +28,99 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   var searchController = TextEditingController();
+
+  void search(BookService bookService) {
+    String keyword = searchController.text;
+    if (keyword.isNotEmpty) {
+      bookService.getBookList(keyword);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: false,
-        title: Text(
-          "Book Store",
-          style: TextStyle(
-              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
-        actions: [
-          Container(
-            alignment: Alignment.bottomCenter,
-            padding: EdgeInsets.only(right: 12),
-            child: Text(
-              "total 40",
+    return Consumer<BookService>(
+      builder: (context, bookService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: false,
+            title: Text(
+              "Book Store",
               style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-              ),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
             ),
-          )
-        ],
-        bottom: PreferredSize(
-          preferredSize: Size(double.infinity, 72),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: searchController,
-              decoration: InputDecoration(
-                hintText: "원하시는 책을 검색해주세요.",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
+            actions: [
+              Container(
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.only(right: 12),
+                child: Text(
+                  "total 40",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
                 ),
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.search),
+              )
+            ],
+            bottom: PreferredSize(
+              preferredSize: Size(double.infinity, 72),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: "원하시는 책을 검색해주세요.",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        search(bookService);
+                      },
+                      icon: Icon(Icons.search),
+                    ),
+                  ),
+                  onSubmitted: (value) {
+                    search(bookService);
+                  },
                 ),
               ),
-              onSubmitted: (value) {},
             ),
           ),
-        ),
-      ),
+          body: bookService.bookList.isEmpty
+              ? Center(
+                  child: Text(
+                    "검색어를 입력해주세요",
+                    style: TextStyle(
+                      fontSize: 21,
+                      color: Colors.grey,
+                    ),
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: bookService.bookList.length,
+                  itemBuilder: (context, index) {
+                    Book book = bookService.bookList[index];
+                    return ListTile(
+                      leading: Image.network(
+                        book.thumbnail,
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(book.title),
+                      subtitle: Text(book.subtitle),
+                    );
+                  },
+                ),
+        );
+      },
     );
   }
 }
